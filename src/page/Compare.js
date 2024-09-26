546e9466d7d944856a006791f12eb0850c06520b
bangsinchur <sinchurbang123@gmail.com>
2024-09-26 17:55:24 +0900

    Fix: CSS Media Query

--- src/page/Compare.js
+++ src/page/Compare.js
@@ -9,11 +9,10 @@ function MyCompare() {
           <p className={compareStyle.headFont}>나의 기업을 선택해 주세요!</p>
         </div>
         <div className={compareStyle.mainSection}>
-          <div className={compareStyle.content}>
-            <img src={btnPlus} alt="btnPlus" />
-            <p className={compareStyle.contentFont}>기업추가</p>
-          </div>
+          <img src={btnPlus} alt="btnPlus" />
+          <p className={compareStyle.contentFont}>기업추가</p>
         </div>
+
         <div className={compareStyle.addSection}>
           <div className={compareStyle.headTheme2}>
             <p className={compareStyle.headFont}> 어떤기업이 궁금하세요?</p>
