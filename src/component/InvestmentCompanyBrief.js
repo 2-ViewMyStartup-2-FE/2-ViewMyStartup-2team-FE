e7f2ffca4b7351cae9fedbae12ab3339dc3eb20d
TaeyeonKim <gggm0117@naver.com>
2024-09-29 20:06:43 +0900

    Implement: SelectedCompanyCard

--- src/component/InvestmentCompanyBrief.js
+++ src/component/InvestmentCompanyBrief.js
@@ -4,7 +4,7 @@ function InvestmentCompanyBrief() {
     <div className={styles.frame}>
       <div className={styles.infoLabel}>투자 기업 정보</div>
       <div className={styles.info}>
-        <img className={styles.logoImg} />
+        <img className={styles.logoImg} alt="로고이미지" />
         <div className={styles.layoutContainer}>
           <p className={styles.companyName}>코드잇</p>
           <p className={styles.companyCategory}>에듀테크</p>
