80307230a0082680ddd393c71973ad903b55d040
shinji530 <shinji53059@gmail.com>
2024-09-30 10:31:58 +0900

    Feat: mock data 구조 변경

--- src/pages/StartupPage.js
+++ src/pages/StartupPage.js
@@ -84,7 +84,7 @@ export default function StartupPage() {
         <div className={style.listHeader}>
           <div className={style.rank}>순위</div>
           <div className={style.company}>기업 명</div>
-          <div className={style.description}>기업 소개</div>
+          <div className={style.compDetail}>기업 소개</div>
           <div className={style.category}>카테고리</div>
           <div className={style.category}>누적 투자 금액</div>
           <div className={style.category}>매출액</div>
