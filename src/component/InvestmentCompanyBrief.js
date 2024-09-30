ff71089b35fdd8590d18d0976bab8a02afa223de
TaeyeonKim <gggm0117@naver.com>
2024-09-30 17:03:40 +0900

    1. Changed media units from rem to px 2. Added scroll to modal when browser window is resized 3. Disabled resizing for textarea 4. Centered the close button image

--- src/component/InvestmentCompanyBrief.js
+++ src/component/InvestmentCompanyBrief.js
@@ -1,10 +1,10 @@
 import styles from "../css/InvestmentCompanyBrief.module.css";
-function InvestmentCompanyBrief() {
+function InvestmentCompanyBrief({ className }) {
   return (
-    <div className={styles.frame}>
+    <div className={`${styles.frame} ${className}`}>
       <div className={styles.infoLabel}>투자 기업 정보</div>
       <div className={styles.info}>
-        <img className={styles.logoImg} />
+        <img className={styles.logoImg} about="로고이미지" />
         <div className={styles.layoutContainer}>
           <p className={styles.companyName}>코드잇</p>
           <p className={styles.companyCategory}>에듀테크</p>
