0a6b4b4cde53db805fa28ab6dd31b24554cb5f83
annylee0723 <annylee0723@gmail.com>
2024-10-02 21:09:48 +0900

    Refactor: #44-Refactor Company Detail Table component

--- src/component/CompanyDetailTable.js
+++ src/component/CompanyDetailTable.js
@@ -1,15 +1,16 @@
 import styles from "../css/CompanyDetailPage.module.css";
 import defaultImg from "../asset/images/img_company_default_logo.png";
 import ConvertBillion from "../utils/ConvertBillion.js";
+
 function CompanyDetailTable({ data }) {
+  const logoSrc = data.logo === "" ? defaultImg : data.logo;
+  const handleImageError = (e) => (e.target.src = defaultImg);
+  const formatNumber = (value) => ConvertBillion(parseInt(value));
+
   return (
     <>
       <div className={styles.company}>
-        <img
-          src={data.logo == "" ? defaultImg : data.logo}
-          alt="Logo"
-          onError={(e) => (e.target.src = defaultImg)}
-        />
+        <img src={logoSrc} alt="Logo" onError={handleImageError} />
         <div className={styles.companyName}>
           <h1>{data.name}</h1>
           <p>{data.category}</p>
@@ -18,11 +19,11 @@ function CompanyDetailTable({ data }) {
       <div className={styles.companyInfo}>
         <div className={styles.companyDetailInfo}>
           <h1>누적 투자 금액</h1>
-          <p>{ConvertBillion(parseInt(data.totalInvestment))} 원</p>
+          <p>{formatNumber(data.totalInvestment)} 원</p>
         </div>
         <div className={styles.companyDetailInfo}>
           <h1>매출액</h1>
-          <p>{ConvertBillion(parseInt(data.revenue))} 원</p>
+          <p>{formatNumber(data.revenue)} 원</p>
         </div>
         <div className={styles.companyDetailInfo}>
           <h1>고용 인원</h1>
