f913ebd315ca06bfba35df9510f824a9eca95ee4
shinji530 <shinji53059@gmail.com>
2024-10-04 14:25:10 +0900

    Feat: 로고 누르면 reload되고 바로 /로 보내도록 변경

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,21 +1,27 @@
 import img_logo from "../asset/images/img_logo.png";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import style from "../css/Nav.module.css";
 
 export default function Nav() {
   const location = useLocation();
+  const navigate = useNavigate();
   const currentPath = location.pathname;
 
   const isCompare = currentPath === "/compare";
   const isCompareStatus = currentPath === "/compare-status";
   const isInvestStatus = currentPath === "/invest-status";
 
+  const handleLogoClick = () => {
+    window.location.reload();
+    navigate("/");
+  };
+
   return (
     <div className={style.header}>
       <div className={style.nav}>
-        <Link to='/'>
-          <img className={style.logo} src={img_logo} alt="logo" />
-        </Link>
+        <div>
+          <img className={style.logo} src={img_logo} alt="logo" onClick={handleLogoClick} />
+        </div>
         <div className={style.titleGroup}>
           <Link
             className={`${style.title} ${isCompare ? style.active : ""}`}
