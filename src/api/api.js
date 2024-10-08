e8239166bdcf9e6764a5b7e52b4e8ce1236c846b
annylee0723 <annylee0723@gmail.com>
2024-10-08 18:08:31 +0900

    fix: #101-feat to develop branch

--- src/api/api.js
+++ src/api/api.js
@@ -2,20 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://two-viewmystartup-2team-be.onrender.com/api",
-  //"http://localhost:3000/api"
   // timeout: 10000,
 });
 
 export async function requestGet(url, params = {}) {
-  try {
-    return await instance.get(url, { params });
-  } catch (e) {
-    console.error("get error: ", e.message);
-  }
-}
-
-//배포된 서버에서 Get으로 데이터 가져오기
-export async function requestGetDB(url, params = {}) {
   try {
     // URL 유효성 검사 (문자열이 맞는지 확인, 공백만 있는 문자열이 아닌지 확인)
     if (typeof url !== "string" || !url.trim()) {
@@ -44,11 +34,9 @@ export async function requestPost(url, data) {
 
 export async function requestPatch(url, data) {
   try {
-    console.log("리퀘스트패치 데이터:", data);
     return await instance.patch(url, data);
   } catch (e) {
     console.error("patch error: ", e.message);
-    throw e; // 에러를 다시 던져서 호출한 곳에서 처리할 수 있도록 함
   }
 }
 
