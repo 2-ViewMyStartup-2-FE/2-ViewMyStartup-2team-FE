22d3cfba2f73484e5c6c6540f336cbae9a2ada9e
TaeyeonKim <gggm0117@naver.com>
2024-10-10 13:09:38 +0900

    Complete

--- src/component/ComparisonTableTitle.js
+++
@@ -1,5 +0,0 @@
-import styles from "../css/ComparisonTableTitle.module.css";
-function ComparisonTableTitle({ children }) {
-  return <div className={styles.title}>{children}</div>;
-}
-export default ComparisonTableTitle;
