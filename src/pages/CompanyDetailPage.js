f2a6e08925ff3d47ba6e1efbb8b15c8a21592556
annylee0723 <annylee0723@gmail.com>
2024-10-11 10:10:18 +0900

    fetch:add dev brancj

--- src/pages/CompanyDetailPage.js
+++ src/pages/CompanyDetailPage.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { getCompanyDetail } from "../api/CompanyDetailAPI.js";
 import styles from "../css/CompanyDetailPage.module.css";
 import InvestmentInfoList from "../component/InvestmentInfoList.js";
@@ -10,26 +10,32 @@ export default function CompanyDetailPage() {
   const [startupData, setStartupData] = useState([]); // 스타트업 데이터 상태 관리
   const { id } = useParams();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getCompanyDetail(id);
-        console.log(response);
-        if (response) {
-          setStartupData(response);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await getCompanyDetail(id);
+      console.log(response);
+      if (response) {
+        setStartupData(response);
       }
-    };
-
-    fetchData();
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   }, [id]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className={styles.companyDetailPage}>
       <CompanyDetailTable startupListData={startupData} />
-      <CompanyInvestmentAction startupListData={startupData} />
-      <InvestmentInfoList data={startupData} />
+      <CompanyInvestmentAction
+        startupListData={startupData}
+        fetchData={fetchData}
+      />
+      <InvestmentInfoList data={startupData} fetchData={fetchData} />
     </div>
+    sdfsdf
+    sdfsdf
   );
 }
